96731afd81ada6684855de3596cead88f482d118
Reimar Bauer <rb.proj@gmail.com>
2018-09-15 21:15:23 +0200

    helper for envs improved

--- recipe/helper.py
+++ recipe/helper.py
@@ -1,6 +1,11 @@
 import os
-import conda
-if os.getenv('CONDA_PREFIX') is None:
+try:
+     import conda
+except ImportError:
+     print(os.getenv('PROJ_LIB'))
+     conda = None
+     
+if conda is not None or os.getenv('CONDA_PREFIX') is None:
     conda_file_dir = conda.__file__
     conda_dir = conda_file_dir.split('lib')[0]
     proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
