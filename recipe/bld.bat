8214da33a9ddc0da0a457b12f71ea98ef83dbdd7
Reimar Bauer <rb.proj@gmail.com>
2022-06-09 11:00:58 +0200

    v7.0.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 set MENU_DIR=%PREFIX%\Menu
 if not exist %MENU_DIR% mkdir %MENU_DIR%
 if errorlevel 1 exit 1
-copy %SRC_DIR%\Menu\mss_shortcut.json %MENU_DIR%\mss_shortcut.json
+copy %SRC_DIR%\Menu\msui_shortcut.json %MENU_DIR%\msui_shortcut.json
 if errorlevel 1 exit 1
 copy %SRC_DIR%\Menu\mss.ico %MENU_DIR%\mss.ico
 if errorlevel 1 exit 1
-%PYTHON% -m pip install . --no-deps -vv
+%PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
