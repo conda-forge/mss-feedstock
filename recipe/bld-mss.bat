3f17b4cb25e1fcd9deb93c6c3d458774813f05a2
ReimarBauer <rb.proj@gmail.com>
2024-02-15 13:04:40 +0100

    update

--- recipe/bld-mss.bat
+++ recipe/bld-mss.bat
@@ -7,8 +7,9 @@ set GEOS_DIR=%LIBRARY_PREFIX%
 
 :: Jump to the correct build section
 if "%PKG_NAME%"=="mss" GOTO CASE_mss
-if "%PKG_NAME%"=="mss-mswms" GOTO CASE_mss_mswms
+if "%PKG_NAME%"=="mss-wms" GOTO CASE_mss_mswms
 if "%PKG_NAME%"=="mss-mscolab" GOTO CASE_mss_mscolab
+if "%PKG_NAME%"=="mss-msui" GOTO CASE_mss_msui
 
 ECHO "Unknown package: %PKG_NAME%"
 EXIT 1
@@ -21,15 +22,20 @@ EXIT 1
   if errorlevel 1 exit 1
   copy %SRC_DIR%\Menu\mss.ico %MENU_DIR%\mss.ico
   if errorlevel 1 exit 1
-  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed -vvv --record record.txt packages\mss
+  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed --record record.txt -vvv  packages\mss
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
 :CASE_mss_mswms
-  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed  -vvv --record record.txt packages\mss_mswms
+  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed --record record.txt -vvv  packages\mss_wms
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
 :CASE_mss_mscolab
-  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed  -vvv --record record.txt packages\mss_mscolab
+  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed --record record.txt -vvv  packages\mss_mscolab
   IF errorlevel 1 EXIT 1
   GOTO CASE_end
+:CASE_mss_msui
+  "%PYTHON%" setup.py install --single-version-externally-managed --ignore-installed --record record.txt -vvv packages\mss_msui
+  IF errorlevel 1 EXIT 1
+  GOTO CASE_end
+
 :CASE_END
