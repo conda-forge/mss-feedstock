88ca6d9291bcf2fc1a173b9350f624cc04d15ad3
Reimar Bauer <rb.proj@gmail.com>
2025-09-03 13:03:21 +0200

    removed menu - for debugging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,10 @@
-mkdir "%PREFIX%\Menu"
-copy /Y "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
-copy /Y "%RECIPE_DIR%\msui.ico" "%PREFIX%\Menu\msui.ico"
+; debug no menu
+;mkdir "%PREFIX%\Menu"
+;copy /Y "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\%PKG_NAME%_menu.json"
+;copy /Y "%RECIPE_DIR%\msui.ico" "%PREFIX%\Menu\msui.ico"
 
 ; conda-build issue 5311, currently we can't build for windows with pip install
 ; %PYTHON% setup.py install --single-version-externally-managed --record record.txt
-%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-;%PYTHON% -m pip install . -vvv
+;%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+%PYTHON% -m pip install . -vvv
 if errorlevel 1 exit 1
