a007b7701c213f0f25147d80218298549edb8220
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2025-09-03 18:37:02 +0200

    Updated v10.2.0 (#198)
    ---------
    
    Co-authored-by: regro-cf-autotick-bot <36490558+regro-cf-autotick-bot@users.noreply.github.com>
    Co-authored-by: Reimar Bauer <rb.proj@gmail.com>
    Co-authored-by: ReimarBauer <rb.proj@googlemail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,4 @@ cp "${RECIPE_DIR}/menu.json" "${PREFIX}/Menu/${PKG_NAME}_menu.json"
 cp "${RECIPE_DIR}/msui.png" "${PREFIX}/Menu/msui.png"
 cp "${RECIPE_DIR}/msui.icns" "${PREFIX}/Menu/msui.icns"
 
-"${PYTHON}" -m pip install . --no-deps -vv
+"${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
