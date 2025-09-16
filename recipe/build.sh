279ce5fb737504650443af09b81f4969c5eaf209
Matthias Riße <9308656+matrss@users.noreply.github.com>
2025-09-16 15:32:20 +0200

    Switch to noarch (#201)
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p "${PREFIX}/Menu"
-cp "${RECIPE_DIR}/menu.json" "${PREFIX}/Menu/${PKG_NAME}_menu.json"
-cp "${RECIPE_DIR}/msui.png" "${PREFIX}/Menu/msui.png"
-cp "${RECIPE_DIR}/msui.icns" "${PREFIX}/Menu/msui.icns"
-
-"${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
